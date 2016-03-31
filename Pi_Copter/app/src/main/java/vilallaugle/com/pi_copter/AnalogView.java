a170a6d311522aa9dee103622a2adfe5392ab512
Nicolas Naugle <Naugle@Nicolass-MacBook-Pro.local>
2016-03-31 02:42:28 -0400

    Moved the touch event to the AnalogView from the activity

--- Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/AnalogView.java
+++ Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/AnalogView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -19,6 +20,15 @@ class AnalogView extends View {
     //locations of the controls, the large circles
     private float analogBasex = 120;
     private float analogBasey = 120;
+
+    private static int leftAnalogBasex = 120;
+    private static int leftAnalogBasey = 120;
+    private static int rightAnalogBasex = 120;
+    private static int rightAnalogBasey = 770;
+
+    boolean down = false;
+
+    double leftTheta;
 
     private static float analogBaseRadius = 100;
     private static float analogBaseRadius2 = 85;
@@ -70,5 +80,49 @@ class AnalogView extends View {
 
         //draw the movable circle that acts as a control
         canvas.drawCircle(analogx, analogy, analogStickRadius, stickpaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float xx = event.getX();
+        float yy = event.getY();
+        //boolean down = false;
+        double tempx = (xx - leftAnalogBasex) * (xx - leftAnalogBasex);
+        double tempy = (yy - leftAnalogBasey) * (yy - leftAnalogBasey);
+        //if you are touching within the circle, change the location
+        if (down == true || (Math.sqrt(tempx + tempy) < 100 && event.getAction() == MotionEvent.ACTION_MOVE)) {
+            //if we press and go out of bounds, then we should keep the control in bounds,
+            //but still be able to follow the direction of the touch
+            xx = event.getX();
+            yy = event.getY();
+            down = true;
+            tempx = (xx - leftAnalogBasex) * (xx - leftAnalogBasex);
+            tempy = (yy - leftAnalogBasey) * (yy - leftAnalogBasey);
+
+            if (Math.sqrt(tempx + tempy) > 100) {
+
+                if (xx >= leftAnalogBasex) {
+                    leftTheta = (Math.atan((yy - leftAnalogBasey) / (xx - leftAnalogBasex)));
+                } else {
+                    leftTheta = (Math.atan((yy - leftAnalogBasey) / (xx - leftAnalogBasex))) + Math.PI;
+                }
+
+                tempy = 100 * Math.sin(leftTheta) + leftAnalogBasey;
+                tempx = 100 * Math.cos(leftTheta) + leftAnalogBasex;
+
+                setPositions((int) tempx, (int) tempy);
+            } else {
+                setPositions((int) xx, (int) yy);
+            }
+        }
+        if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+            setPositions(leftAnalogBasex, leftAnalogBasey);
+            down = false;
+        }
+
+        invalidate();
+        return true;
+        //return super.onTouchEvent(event);
     }
 }
--- Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/MainActivity.java
+++ Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/MainActivity.java
@@ -15,13 +15,12 @@ import java.lang.Math;
 import android.support.v4.view.GestureDetectorCompat;
 
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
-        GestureDetector.OnDoubleTapListener {
+public class MainActivity extends AppCompatActivity {
 
     private static int leftAnalogBasex = 120;
     private static int leftAnalogBasey = 120;
-    private int touchx;
-    private int touchy;
+    private static int rightAnalogBasex = 120;
+    private static int rightAnalogBasey = 770;
 
     //angle of the left press from the left analog base
     double leftTheta;
@@ -37,139 +36,11 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         //setContentView(R.layout.activity_main);
         //final ControlView control = new ControlView(this, 0, 0, 0, 0);
         final AnalogView leftAnalog = new AnalogView(this, leftAnalogBasex, leftAnalogBasey);
+        //final AnalogView rightAnalog = new AnalogView(this, rightAnalogBasex, rightAnalogBasey);
         leftAnalog.setBackgroundColor(Color.BLACK);
         setContentView(leftAnalog);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*
-
-            }
-        });*/
-        leftAnalog.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                //get the locations of the touch event and pass it into the view.
-                float xx = event.getX();
-                float yy = event.getY();
-                //boolean down = false;
-                double tempx = (xx-leftAnalogBasex)*(xx-leftAnalogBasex);
-                double tempy = (yy-leftAnalogBasey)*(yy-leftAnalogBasey);
-                //if you are touching within the circle, change the location
-                if (down == true || (Math.sqrt(tempx + tempy) < 100 && event.getAction() == MotionEvent.ACTION_MOVE)) {
-                    //if we press and go out of bounds, then we should keep the control in bounds,
-                    //but still be able to follow the direction of the touch
-                    xx = event.getX();
-                    down = true;
-                    yy = event.getY();
-                    tempx = (xx-leftAnalogBasex)*(xx-leftAnalogBasex);
-                    tempy = (yy-leftAnalogBasey)*(yy-leftAnalogBasey);
-
-                    if (Math.sqrt(tempx + tempy) > 100) {
-
-                        if (xx >= leftAnalogBasex) {
-                            leftTheta = (Math.atan((yy - leftAnalogBasey) / (xx - leftAnalogBasex)));
-                        } else {
-                            leftTheta = (Math.atan((yy - leftAnalogBasey) / (xx - leftAnalogBasex))) + Math.PI;
-                        }
-                        System.out.println("got here too");
-
-                        tempy = 100*Math.sin(leftTheta) + leftAnalogBasey;
-                        tempx = 100*Math.cos(leftTheta) + leftAnalogBasex;
-
-                        leftAnalog.setPositions((int) tempx, (int) tempy);
-                    } else {
-                        leftAnalog.setPositions((int) xx, (int) yy);
-                    }
-                }
-                if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
-                    leftAnalog.setPositions(leftAnalogBasex, leftAnalogBasey);
-                    down = false;
-                }
-                return true;
-            }
-        });
-        leftAnalog.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                return false;
-            }
-        });
-    }
-
-
-
-
-    /////////////////////// BEGIN GESTURES ////////////////////////////////
-    @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-
-        return true;
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
 
     }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-
-    /////////////////////// END GESTURES ////////////////////////////
-
-    /*
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        touchx = (int)event.getX();
-        touchy = (int)event.getY();
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
-            case MotionEvent.ACTION_UP:
-        }
-        //control.setPositions(x, y, 0, 0);
-        //return super.onTouchEvent(event);
-        return false;
-    }
-*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
