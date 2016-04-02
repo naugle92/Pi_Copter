a6c005ff3408b0149caf6f714b1fcd8b179fdb8e
Nicolas Naugle <Naugle@Nicolass-MacBook-Pro.local>
2016-04-02 02:01:16 -0400

    Clear button on the home screen works

--- Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/ConnectionActivity.java
+++ Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/ConnectionActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ConnectionActivity extends AppCompatActivity {
 
@@ -18,5 +19,12 @@ public class ConnectionActivity extends AppCompatActivity {
         // Do something in response to button
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void clearMessage(View view) {
+        EditText text = (EditText) findViewById(R.id.ipAddress);
+        text.setText("");
+        text = (EditText) findViewById(R.id.port);
+        text.setText("");
     }
 }
