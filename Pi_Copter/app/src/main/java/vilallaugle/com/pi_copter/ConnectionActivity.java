cb11a05cea120ba3e3d60b3cc2c94b34dd0bea43
Nicolas Naugle <Naugle@Nicolass-MacBook-Pro.local>
2016-04-14 03:26:48 -0400

    Start of the SocketIO

--- Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/ConnectionActivity.java
+++ Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/ConnectionActivity.java
@@ -3,15 +3,39 @@ package vilallaugle.com.pi_copter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+//import java.io.DataOutputStream;
+//import java.io.IOException;
+//import java.net.Socket;
+import java.net.URISyntaxException;
+
 public class ConnectionActivity extends AppCompatActivity {
 
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://172.28.82.44:2008");
+        } catch (URISyntaxException e) {
+
+        }
+    }
+    /*Socket socket = null;
+    socket = new Socket("http://172.28.81.67",2004);
+    DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
+    DOS.writeUTF("HELLO_WORLD");
+    socket.close();
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+        mSocket.connect();
     }
 
     /** Called when the user clicks the Send button */
@@ -26,5 +50,18 @@ public class ConnectionActivity extends AppCompatActivity {
         text.setText("");
         text = (EditText) findViewById(R.id.port);
         text.setText("");
+        String message = "Fuck this socket shit\n";
+        mSocket.emit("new message", message);
+    }
+
+    private void attemptSend() {
+        /*String message = mInputMessageView.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+
+        mInputMessageView.setText("");
+        mSocket.emit("new message", message);*/
+        System.out.println("ATTEMPTING TO SEND\n");
     }
 }
--- Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/MainActivity.java
+++ Pi_Copter/app/src/main/java/vilallaugle/com/pi_copter/MainActivity.java
@@ -2,17 +2,9 @@ package vilallaugle.com.pi_copter;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.GestureDetector;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import java.lang.Math;
-import android.support.v4.view.GestureDetectorCompat;
 
 
 public class MainActivity extends AppCompatActivity {
